ebaf319fb100de0c92022ab54e4e8fb841de06d3
Nicolás Zicarelli <nico@argensonix.com.ar>
2024-08-14 18:09:08 -0300

    Rollback, Heroku temporarily disabled

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 
-export default defineConfig({
-  output: 'server', 
-  adapter: node({
-    mode: 'standalone', 
-  }),
-  site: process.env.SITE_URL || 'https://uxipsos-astra.herokuapp.com',  // Usar variable de entorno si está disponible
-});
+// https://astro.build/config
+export default {
+    // Otras configuraciones aquí
+    buildOptions: {
+        baseUrl: '/' // Ruta base personalizada
+    }
+}
