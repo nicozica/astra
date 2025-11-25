bbf677789bfa6625006fd509f1f339f40490455e
Nicolás Zicarelli <nico@argensonix.com.ar>
2025-11-25 17:27:27 -0300

    Added allowed hosts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default {
-    // Otras configuraciones aquí
-    buildOptions: {
-        baseUrl: '/' // Ruta base personalizada
-    }
-}
+export default defineConfig({
+  // Otras configuraciones aquí
+  buildOptions: {
+    baseUrl: '/', // Ruta base personalizada
+  },
+  vite: {
+    preview: {
+      allowedHosts: [
+        'uxipsos-astra-84a1bc8bfda0.herokuapp.com',
+        '.herokuapp.com',
+        'localhost',
+      ],
+    },
+  },
+});
